b43fe84c4cb1f7eb14398f6efe93c70810ef973b
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-08-09 15:14:08 +0200

    Added Freepik attribution in footer

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,17 +19,18 @@ function Home() {
     return (
         <Container
             fluid
-            className="d-flex justify-content-center align-items-center" // Remove padding and margin
+            className="d-flex justify-content-center align-items-center p-0 px-2" // Remove padding and margin
             style={{
                 backgroundImage: `url(${backgroundImage})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 width: '100vw',
                 height: '100vh',
-                overflow: 'hidden' // Ensure no overflow issues
+                overflow: 'hidden', // Ensure no overflow issues
+                flexDirection: 'column'
             }}
         >
-            <Col xs={12} sm={10} md={8} xl={6} className="d-flex flex-column border border-5 border-dark-subtle rounded-3 p-3 ps-4 bg-white mx-0 mx-sm-2 mx-md-2" style={{maxWidth: '640px'}}>
+            <Col xs={12} sm={10} md={8} xl={6} className="d-flex flex-column border border-5 border-dark-subtle rounded-3 p-3 ps-4 bg-white mx-1 mx-sm-2 mx-md-2 mt-auto" style={{maxWidth: '640px'}}>
                 <Col className="mb-3 d-flex gap-2 row">
                     <h1 className="mb-0 fw-normal">Grzegorz Gajewski</h1>
                     <h5 className="mb-0 fw-light">Third year student of computer science</h5>
@@ -51,9 +52,12 @@ function Home() {
                     </Button>
                 </Col>
             </Col>
+            <footer className="text-end mt-auto d-flex justify-content-end align-items-center bg-white align-self-end me-0 pe-1" style={{fontSize: "15px", fontFamily: "Fredoka"}}>
+                    <p className="m-0 me-1" >Background by</p><a href={"https://www.freepik.com/"}>Freepik</a>
+            </footer>
             <Modal centered show={show} onHide={handleClose}>
                 <Modal.Body>
-                    <Col>
+                    <Col className="">
                         Please visit my LinkedIn profile or Resume to obtain contact info.
                     </Col>
                 </Modal.Body>
